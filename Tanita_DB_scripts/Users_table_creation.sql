c5bd869b08a617e80bd31d289d8a66c0fb1a640e
AlvaroArellano94 <alvaro.arellano@datua.ai>
2024-10-26 20:17:55 +0200

    He quitado de dentro de la tabla de Usuarios el nombre del fichero. Posteriormente, voy a crear una tabla intermedia entre ambos.

--- Tanita_DB_scripts/Users_table_creation.sql
+++ Tanita_DB_scripts/Users_table_creation.sql
@@ -1,4 +1,5 @@
-#CREATE DATABASE Tanita_app_db;
+--CREATE DATABASE Tanita_app_db;
+--this is only used when we need to create the database for the first time
 USE Tanita_app_db;
 
 CREATE TABLE IF NOT EXISTS Users (
@@ -8,8 +9,9 @@ CREATE TABLE IF NOT EXISTS Users (
     Gender VARCHAR(255),
     Age TINYINT NOT NULL,
     Height DECIMAL(3,2) NOT NULL,
-    File_name VARCHAR(100) NOT NULL,
+    --File_name VARCHAR(100) NOT NULL, --this field does not depends on the User. So I am going to create a 
+    --intermidate table "User_File_Name" in order to normalize this table
     CONSTRAINT CHK_GENDER CHECK (Gender="Male" or Gender="Female" or Gender="Other" or Gender="NA")
 );
 
-#drop table users;
+--drop table users;
